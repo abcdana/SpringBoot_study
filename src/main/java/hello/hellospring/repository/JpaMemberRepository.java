3c609b8f1fc0595be9254c297a73ef122b312917
abcdana <74163296+abcdana@users.noreply.github.com>
2021-04-23 19:59:34 +0900

    Edit JPA entity mapping

--- src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -4,21 +4,27 @@ import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 public class JpaMemberRepository implements MemberRepository {
+
     private final EntityManager em;
+
     public JpaMemberRepository(EntityManager em) {
         this.em = em;
     }
+
     public Member save(Member member) { em.persist(member);
         return member;
     }
+
     public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
     }
+
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
+
     public Optional<Member> findByName(String name) {
         List<Member> result = em.createQuery("select m from Member m where
                 m.name = :name", Member.class)
@@ -26,4 +32,5 @@ public class JpaMemberRepository implements MemberRepository {
                 .getResultList();
         return result.stream().findAny();
     }
+
 }
